553f6fa7adc7b7e24380bd4d30a741b24251f262
HABEEB <habeeb.study@gmail.com>
2024-02-19 17:55:01 +0530

    OK ADDED

---
+++ Add2nums.java
@@ -0,0 +1,19 @@
+class Add2nums {
+    int a ;
+    int b ;
+    int L  ;
+
+    public Add2nums(int a, int b, int L) {
+        this.a = a;
+        this.b = b;
+        this.L = L;
+    }
+
+    public static void main(String[] args) {
+
+        Add2nums ADDED = new Add2nums(20,600,500);
+
+        System.out.println(ADDED.a);
+        System.out.println(ADDED.a+ ADDED.b+ ADDED.L);
+    }
+}
