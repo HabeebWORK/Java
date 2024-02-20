0c5eaad1ecafd2674eaf3c6c2aad58a1f483d000
HABEEB <habeeb.study@gmail.com>
2024-02-20 15:16:43 +0530

    STILL TRYING......

--- PACK2_FIRST_PROGRAMS/Even_Odd.java
+++ PACK2_FIRST_PROGRAMS/Even_Odd.java
@@ -1,2 +1,19 @@
-package PACK2_FIRST_PROGRAMS;public class Even_Odd {
+package PACK2_FIRST_PROGRAMS;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a Number to check whether a Number is Even or Odd :");
+        Scanner Number = new Scanner(System.in);
+
+        int num = Number.nextInt();
+
+        if(num%2==0){
+            System.out.println("Yes" + " " + num + " " + "is a Even number");
+        }else {
+            System.out.println("Yes" + " " + num + " " + "is a Odd number");
+        }
+    }
 }
